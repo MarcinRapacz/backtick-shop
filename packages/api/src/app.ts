d0f670e081f66612ace231a608928d7004a3097f
MarcinRapacz <marcin_04@onet.eu>
2022-04-18 13:18:25 +0200

    build: implement eslint

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,13 +1,14 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
 const port: number = 3001;
 
-app.get("/toto", (req: Request, res: Response) => {
-  res.send("Hello toto");
+app.get('/toto', (req: Request, res: Response) => {
+  res.send('Hello toto');
 });
 
-app.listen(port, function () {
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App is listening on port ${port} !`);
 });
